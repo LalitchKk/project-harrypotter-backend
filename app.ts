3be4b00462d3526bd59124787142f3ddbbb3c1f7
LalitchKk <lalikakhon@gmail.com>
2024-03-10 15:58:09 +0700

    open-member-file

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { router as index } from "./api/index";
+import { router as member } from "./api/member";
 
 export const app = express();
 
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use("/", index);
-// app.use("/member", member);
+app.use("/member", member);
 // app.use("/", (req, res) => {
 //   res.send("Hello World!!!");
 // });
