b6c988b2973943ed94efe7c140883b7c05e1131e
LalitchKk <lalikakhon@gmail.com>
2024-03-10 15:53:13 +0700

    100324-harry-back

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 export const router = express.Router();
 const currentDate: Date = new Date();
 router.get('/', (req, res)=>{
-    res.render('Get in index.ts'+currentDate);
+    res.send('Get in index.ts'+currentDate);
 });
